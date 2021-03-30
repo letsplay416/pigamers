ed17f4c3018c40d0c76a7e46983cd752fd104007
PoisoNels Windows <letsplay.app.game@gmail.com>
2021-03-30 15:08:32 +0200

    ranking screen

--- lib/src/views/ui/screens/main_screens/ranking_screen.dart
+++ lib/src/views/ui/screens/main_screens/ranking_screen.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pigamers/src/views/utils/constants.dart';
 
 class RankingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+            expandedHeight: Get.size.height * 0.3,
+            backgroundColor: kPrimaryColor.withOpacity(0.2),
+            stretch: true,
+            centerTitle: true,
+            floating: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("Ranking Screen"),
+              stretchModes: [
+                StretchMode.zoomBackground,
+                StretchMode.fadeTitle,
+                StretchMode.blurBackground
+              ],
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    "https://htmlcolorcodes.com/assets/images/html-color-codes-color-tutorials-hero.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                  DecoratedBox(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment(0.0, 0.5),
+                              colors: [
+                                kPrimaryColor.withOpacity(0.3),
+                                Color(0x00000000)
+                              ],
+                              end: Alignment(0.0, 0.0))))
+                ],
+              ),
+              centerTitle: true,
+            ),
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            Container(
+              color: kFourthColor,
+              height: 999,
+            ),
+          ]))
+        ],
+      ),
     );
   }
 }
